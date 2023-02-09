892f8e2a4944b62741f1bd1ec972e85bd89efecf
kineticneticat <84404038+kineticneticat@users.noreply.github.com>
2023-02-09 21:49:32 +0000

    aaa

--- static/js/Tilemaps.mjs
+++ static/js/Tilemaps.mjs
@@ -1,12 +1,18 @@
 import {ImageHandler} from '../js/Images.mjs'
+<<<<<<< HEAD
 import {player} from '../js/Player.mjs'
 import {Vec2d} from '../js/Math.mjs'
+=======
+>>>>>>> 4648f2e (aaa)
 
 class Tilemap {
 	constructor(path) {
 		this.path = path
 		this.img = new ImageHandler(path)
+<<<<<<< HEAD
 		this.imgCentreOffset = new Vec2d(this.img.width/2, this.img.height/2)
+=======
+>>>>>>> 4648f2e (aaa)
 	}
 	draw() {
 		this.img.draw(player.pos.inverse)
